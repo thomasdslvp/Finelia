7fbdf1204c1383b2b63a71065c981eb133e4b7fb
thomasdslvp <ths.dslvp@gmail.com>
2021-05-10 16:32:24 +0200

    ajout des notes dans la bdd
    Ce qu'il manque :
    - Calcul de la moyenne des étudiants
    - Améliorer l'esthétique

--- formulaire.php
+++ formulaire.php
@@ -1,37 +1,57 @@
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Thomas DA SILVA PENAS - Test d'admission FINELIA</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
 
-    <body>
-        
+<head>
+    <meta charset="utf-8">
+    <title>Thomas DA SILVA PENAS - Test d'admission FINELIA</title>
+    <link rel="stylesheet" href="style/style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/javascript.js"></script>
+</head>
+
+<body>
+    <div id="connexion-bd">
         <!--Test de la connexion à la BD -->
-        <?php   $servername = 'localhost';
-                $login = 'root';
-                $pwd = '';
+        <?php $servername = 'localhost';
+        $login = 'root';
+        $pwd = '';
 
-                $connexion = new PDO("mysql:host=$servername;dbname=finelia_db", $login, $pwd);
-                if(!$connexion){
-                    die('Erreur : ' .mysqli_connect_error());
-                }
-                echo 'Connexion réussie';
-                
-                //Requêtes de récupération des données dans la base MySQL
-                $etudiants = $connexion->query('SELECT * FROM etudiant');
-                $matières = $connexion->query('SELECT * FROM matiere');
-        ?>  
-            <form action="moyenne.php" method="post">
-                <p>Rentrez les notes des étudiants suivants :</p>
-        <?php
-                while ($etudiant = $etudiants->fetch()){
+
+        $connexion = new PDO("mysql:host=$servername;dbname=finelia_db", $login, $pwd);
+        if (!$connexion) {
+            die('Erreur : ' . mysqli_connect_error());
+        }
+        echo 'Connexion à la base réussie';
+
+        //Requêtes de récupération des données dans la base MySQL
+        $etudiants = $connexion->query('SELECT * FROM etudiant');
+        $matières = $connexion->query('SELECT * FROM matiere');
         ?>
-                    <p><?php echo $etudiant['nom'] . " " . $etudiant['prenom'];?>
+    </div>
+    <form action="moyenne.php" method="post">
+        <p>Choisissez un étudiant, une matière et rentrez la note ainsi que le coef :</p>
+        <select name="etudiant">
+            <?php
+            while ($etudiant = $etudiants->fetch()) { ?>
+                <option value=<?php echo "" . $etudiant['id_etudiant']. ""?>>
+                <?php echo $etudiant['nom'] . " " . $etudiant['prenom'];?></option>
+            <?php
+            } ?>           
+        </select>
+        
+        <select name="matiere">
+        <?php
+            while ($matiere = $matières->fetch()) { ?>
+                <option value=<?php echo "" . $matiere['id_matiere']. ""?>>
+                <?php echo $matiere['nom_matiere'];?></option>
+            <?php
+            } ?>
+        </select>
+        <p>Note : <input type="text" name="note"> </p>
+        <p>Coef : <input type="text" name="coef"> </p>
 
-                    </p>
-          <?php } ?>
-                <p><input type="submit" value="OK"></p>
-            </form>
-    </body>
+        <p><input type="submit" value="Valider" id="submitBtn"></p>
+    </form>
+
+</body>
+
 </html>
--- moyenne.php
+++ moyenne.php
@@ -1,3 +1,12 @@
 <?php
-    echo "ça marche";
+include 'formulaire.php';
+    $sql = "INSERT INTO note(note, coef, id_matiere, id_etudiant) VALUES ($_POST[note], $_POST[coef], $_POST[matiere], $_POST[etudiant])";
+
+    try{
+        $connexion->exec($sql);
+    }
+    catch(PDOException $e){
+        echo "Erreur : " . $e->getMessage();
+    }
 ?>
+<button>Calculer la moyenne des étudiants</button>
