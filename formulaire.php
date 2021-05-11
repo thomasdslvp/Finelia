af9c2a53bd5b441c82e978b98c7fbd7847b1dacc
thomasdslvp <ths.dslvp@gmail.com>
2021-05-11 18:57:56 +0200

    End test
    Projet terminé.
    Améliorations possibles : calcul de la moyenne pour chaque matière, classement et autres.

--- formulaire.php
+++ formulaire.php
@@ -1,64 +1,64 @@
 <html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Thomas DA SILVA PENAS - Test d'admission FINELIA</title>
+        <link rel="stylesheet" href="style/style.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="js/javascript.js"></script>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">   
+    </head>
 
-<head>
-    <meta charset="utf-8">
-    <title>Thomas DA SILVA PENAS - Test d'admission FINELIA</title>
-    <link rel="stylesheet" href="style/style.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="js/javascript.js"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">   
-</head>
+    <body>
+        <div id="connexion-bd">
+            <!--Test de la connexion à la BD -->
+            <?php $servername = 'localhost';
+            $login = 'root';
+            $pwd = '';
 
-<body>
-    <div id="connexion-bd">
-        <!--Test de la connexion à la BD -->
-        <?php $servername = 'localhost';
-        $login = 'root';
-        $pwd = '';
+            //Connexion
+            try{
+                $connexion = new PDO("mysql:host=$servername;dbname=finelia_db", $login, $pwd);
+                echo 'Connexion à la base réussie';
+            } 
+            catch(Exception $e){
+                die('Erreur : ' . $e->getMessage());
+            }
 
-        //Connexion
-        try{
-            $connexion = new PDO("mysql:host=$servername;dbname=finelia_db", $login, $pwd);
-            echo 'Connexion à la base réussie';
-        } 
-        catch(Exception $e){
-            die('Erreur : ' . $e->getMessage());
-        }
+            //Requêtes de récupération des données dans la base MySQL
+            $etudiants = $connexion->query('SELECT * FROM etudiant');
+            $matières = $connexion->query('SELECT * FROM matiere');
+            ?>
+        </div>
+        
+        <form action="moyenne.php" method="post">
+            <p id="consigne">Choisissez un étudiant, une matière et rentrez la note ainsi que le coefficient :</p>
 
-        //Requêtes de récupération des données dans la base MySQL
-        $etudiants = $connexion->query('SELECT * FROM etudiant');
-        $matières = $connexion->query('SELECT * FROM matiere');
-        ?>
-    </div>
-    
-    <form action="moyenne.php" method="post">
-        <p id="consigne">Choisissez un étudiant, une matière et rentrez la note ainsi que le coefficient :</p>
-        <!-- Liste déroulante de tous les étudiants -->
-        <select name="etudiant">
+            <!-- Liste déroulante de tous les étudiants -->
+            <select name="etudiant">
+                <?php
+                while ($etudiant = $etudiants->fetch()) { ?>
+                    <option value=<?php echo "" . $etudiant['id_etudiant']. ""?>>
+                    <?php echo $etudiant['nom'] . " " . $etudiant['prenom'];?></option>
+                <?php
+                } ?>           
+            </select>
+
+            <!-- Liste déroulante de toutes les matières -->
+            <select name="matiere">
             <?php
-            while ($etudiant = $etudiants->fetch()) { ?>
-                <option value=<?php echo "" . $etudiant['id_etudiant']. ""?>>
-                <?php echo $etudiant['nom'] . " " . $etudiant['prenom'];?></option>
-            <?php
-            } ?>           
-        </select>
-        <!-- Liste déroulante de toutes les matières -->
-        <select name="matiere">
-        <?php
-            while ($matiere = $matières->fetch()) { ?>
-                <option value=<?php echo "" . $matiere['id_matiere']. ""?>>
-                <?php echo $matiere['nom_matiere'];?></option>
-            <?php
-            } ?>
-        </select>
-        <!-- Saisie clavier de la note et du coef -->
-        <p>Note : <input type="text" name="note"> </p>
-        <p>Coef : <input type="text" name="coef"> </p>
+                while ($matiere = $matières->fetch()) { ?>
+                    <option value=<?php echo "" . $matiere['id_matiere']. ""?>>
+                    <?php echo $matiere['nom_matiere'];?></option>
+                <?php
+                } ?>
+            </select>
+            
+            <!-- Saisie clavier de la note et du coef -->
+            <p>Note : <input type="text" name="note"> </p>
+            <p>Coef : <input type="text" name="coef"> </p>
 
-        <p><input type="submit" value="Valider" id="submitBtn"></p>
-    </form>
-
-</body>
-
+            <p><input type="submit" value="Valider" id="submitBtn"></p>
+        </form>
+    </body>
 </html>
--- moyenne.php
+++ moyenne.php
@@ -1,33 +1,40 @@
 <?php
+include('formulaire.php');
+//Insertion des données dans la base
+$sql = "INSERT INTO note(note, coef, id_matiere, id_etudiant) VALUES ($_POST[note], $_POST[coef], $_POST[matiere], $_POST[etudiant])";
 
-include 'formulaire.php';
-    $sql = "INSERT INTO note(note, coef, id_matiere, id_etudiant) VALUES ($_POST[note], $_POST[coef], $_POST[matiere], $_POST[etudiant])";
+try {
+    $connexion->exec($sql);
+} 
+catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
 
-    try{
-        $connexion->exec($sql);     
+//Affichage de tout les étudiants
+$etudiants = $connexion->query('SELECT * FROM etudiant');
+while ($etudiant = $etudiants->fetch()) {
+    $notes = $connexion->query("SELECT * FROM note WHERE id_etudiant = " . $etudiant['id_etudiant']);
+
+    //Calcul de la somme des coefficient pour chaque étudiant
+    $stmt = $connexion->prepare("SELECT SUM(coef) as coefsum FROM note WHERE id_etudiant = " . $etudiant['id_etudiant']);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $coefsum = $row['coefsum'];
+    $notesum = NULL;
+
+    //Calcul de la somme des produits entre les notes et leur coefficient respectifs
+    while ($note = $notes->fetch()) {
+        foreach ($note as $n) :
+            intval($notesum += ($note['note'] * $note['coef']) / 10); // --> somme des notes avec coefficients appliqués
+        endforeach;
     }
-    catch(PDOException $e){
-        echo "Erreur : " . $e->getMessage();
-    }
 
-    $etudiants = $connexion->query('SELECT * FROM etudiant');
-    
+    //Affichage des étudiants ainsi que de leur notes?>
+    <p id="affiche-moyenne"><?php echo "" . $etudiant['nom'] . " "  . $etudiant['prenom'] . " ";
 
-    while ($etudiant = $etudiants->fetch()) {
-        $notes = $connexion->query("SELECT * FROM note WHERE id_etudiant = " . $etudiant['id_etudiant']);
-        $stmt = $connexion->prepare("SELECT SUM(coef) as coefsum FROM note WHERE id_etudiant = " . $etudiant['id_etudiant']);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $coefsum=$row['coefsum'];
-        //DEBUG 
-        print_r($coefsum); // --> Somme des coefficients d'un étudiant
-        while($note = $notes->fetch()){     
-
-            
-        }
-        //$notes = $connexion->exec('SELECT * FROM note WHERE id_etudiant =  ' . $etudiant['id_etudiant']);
-        echo "" . $etudiant['nom']. " "  . $etudiant['prenom']. "";?> </br><?php    
-    } 
-    ?>
-
-<!-- Action à définir -->
+        if (isset($coefsum))
+            print("a " . $notesum / $coefsum . " de moyenne générale");
+        else
+            print("n'a aucune note pour l'instant"); ?></p>
+    </br><?php
+}
